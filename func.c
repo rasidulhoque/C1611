7bd01fe53cccb8c681187cbcc1825e79b2d110de
RASIDUL HOQUE <77561833+rasidulhoque@users.noreply.github.com>
2021-01-26 19:14:03 +0530

    Create func.c
    program to print the sum of n natural numbers using function

---
+++ func.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int sum(int);
+int main()
+//function declaration
+{
+    /*program to print the sum of n natural number using recurcive function*/
+    int k;
+    k=sum(3);
+    printf("%d",k);
+    return 0;
+}
+int sum(int a)
+{
+    int s;
+    if(a==1)
+    return a;
+    s=a+sum(a-1);
+    return s;
+}
