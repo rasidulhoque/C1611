61f60f53d135713a43df68af696b3d9138e7f071
RASIDUL HOQUE <77561833+rasidulhoque@users.noreply.github.com>
2021-03-27 23:28:23 +0530

    functions

---
+++ functions/func2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+//functions declaration
+int sum(int ,int );
+int main()
+{
+    int num1,num2,total=0;
+    printf("enter two number:");
+    scanf("%d %d",&num1,&num2);
+    total=sum(num1,num2);
+    printf("sumation of the two numbers are:%d",total);
+    return 0;
+}
+// function defination
+int sum(int a, int b)
+{
+    int result ;
+    result= a + b;
+    return result;
+}
